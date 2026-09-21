aec0254a4129d9af79d98002a403632ef8f8779c
valeravibrator <crfyz123@gmail.com>
2026-09-21 20:42:40 +0500

    Delete tasks-api directory

--- tasks-api/go.mod
+++
@@ -1,5 +0,0 @@
-module example.com/tasks
-
-go 1.27
-
-require github.com/jackc/pgx/v5 v5.7.6
--- tasks-api/seed.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO tasks (title, done) VALUES
-    ('Сверстать карточку', FALSE),
-    ('Написать хендлер', FALSE),
-    ('Подключить бэк к фронту', TRUE);
